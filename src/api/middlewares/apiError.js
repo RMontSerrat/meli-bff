bd89047858e4404420a736329ada33e3a9bc5800
Romulo Mont Serrat <romulomontserrat@gmail.com>
2020-10-27 15:29:52 -0300

    apiError instance

---
+++ src/api/middlewares/apiError.js
@@ -0,0 +1,27 @@
+class ExtendableError extends Error {
+  constructor({
+    message, errors, status, stack,
+  }) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.errors = errors;
+    this.status = status;
+    this.stack = stack;
+  }
+}
+
+class APIError extends ExtendableError {
+  constructor({
+    message,
+    errors,
+    stack,
+    status = 500,
+  }) {
+    super({
+      message, errors, status, stack,
+    });
+  }
+}
+
+module.exports = APIError;
