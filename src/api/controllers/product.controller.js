b94ef2b199e436ef0dbd074f52cf451cc4e56e12
Romulo Mont Serrat <romulomontserrat@gmail.com>
2020-11-02 17:25:22 -0300

    remove console

--- src/api/controllers/product.controller.js
+++ src/api/controllers/product.controller.js
@@ -28,7 +28,6 @@ const search = async (req, res, next) => {
 const item = async (req, res, next) => {
   const { id } = req.params;
   const result = await getItem(id);
-  console.log(result, 'result');
   const normalizedResult = normalizeProduct(result);
   await itemValidation.validateAsync(normalizedResult);
 
